10e1620a8cc1618cbb00e5f962d38f48e6403a4a
annpimka <pppimka@gmail.com>
2026-04-05 22:28:18 +0300

    create security commons module

---
+++ security-commons/src/main/java/com/ua/petadoption/commons/security/UserHeaders.java
@@ -0,0 +1,10 @@
+package com.ua.petadoption.commons.security;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserHeaders {
+
+    public static final String USER_ID = "X-User-Id";
+    public static final String USER_ROLES = "X-User-Roles";
+}
